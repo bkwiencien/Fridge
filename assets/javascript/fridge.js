5dba7c421eb0105bef5bf9c2056d395a15c6ee46
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-08 13:00:37 -0400

    Aug8-13:00

--- assets/javascript/fridge.js
+++ assets/javascript/fridge.js
@@ -1,11 +1,13 @@
 
-    $(document).ready(function() {
+  function  initialize() {
+       console.log("in initialize");
 
       // Here we are provided an initial array of letters.
       // Use this array to dynamically create buttons on the screen.
       var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "_"];
 
       for (i = 0; i < letters.length; i ++ ) {
+          console.log("in for loop")
           letters[i];
 
           var letterBtn = $("<button>");
@@ -15,4 +17,4 @@
           letterBtn.text(letters[i]);
           $("#buttons").append(letterBtn);
         }
-       }); 
+      }
